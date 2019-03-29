709d461b2b131d15400614189217d468d34d61f9
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2019-03-29 18:20:45 +0100

    minor correction

--- sidebar-footerfull.php
+++ sidebar-footerfull.php
@@ -21,21 +21,6 @@ $container = get_theme_mod( 'theme_layout_container', 'container' );
 
 				<?php dynamic_sidebar( 'footerfull' ); ?>
 
-<div class="row">
-
-				<div id="text-3" class="footer-widget widget_text widget-count-1 col-md-12 mt-4">			<div class="textwidget"><div class="page" title="Page 3">
-<div class="section">
-<div class="layoutArea">
-<div class="column">
-<p>© 2018 Clovis Enterprise Limited registered in England and Wales. Company Number: 11087425. Registered Office: 10 Weyacres, Borrowash, Derbyshire, England, DE72 3QT</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-		</div><!-- .footer-widget -->
-			</div>
-
 			</div>
 
 		</div>
