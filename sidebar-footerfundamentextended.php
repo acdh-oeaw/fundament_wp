432e391dbd65b9e018d6bf9ef52c1b1d4d6b323d
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2021-05-05 16:45:39 +0200

    fundament extended footer translation

--- sidebar-footerfundamentextended.php
+++ sidebar-footerfundamentextended.php
@@ -13,7 +13,7 @@ $container = get_theme_mod('theme_layout_container', 'container');
     <div class="wrapper fundament-default-footer" id="wrapper-footer-full">
         <div class="container" id="footer-full-content" tabindex="-1">
             <div class="footer-separator">
-                <i data-feather="message-circle"></i> CONTACT
+                <i data-feather="message-circle"></i> <?php _e( 'footer_contact', 'fundamentwp' ); ?>
             </div>
             <div class="row">
                 <div class="footer-widget col-lg-1 col-md-2 col-sm-2 col-xs-6 col-3">
@@ -24,21 +24,15 @@ $container = get_theme_mod('theme_layout_container', 'container');
                 <div class="footer-widget col-lg-3 col-md-3 col-sm-6 col-9">
                     <div class="textwidget custom-html-widget">
                         <p>
-                            ACDH-CH
-                            <br>
-                            Austrian Centre for Digital Humanities <br/> and Cultural Heritage
-                            <br>
-                            Austrian Academy of Sciences
+                            <?php _e( 'footer_acdh_text', 'fundamentwp' ); ?>
                         </p>
                         <p>
-                            Sonnenfelsgasse 19,
-                            <br>
-                            1010 Vienna
+                            <?php _e( 'footer_acdh_address', 'fundamentwp' ); ?>
                         </p>
                         <p>
-                            T: +43 1 51581-2200
+                            T: <?php _e( 'footer_tel', 'fundamentwp' ); ?>
                             <br>
-                            E: <a href="mailto:acdh@oeaw.ac.at">acdh@oeaw.ac.at</a>
+                            E: <a href="mailto:<?php _e( 'footer_email', 'fundamentwp' ); ?>"><?php _e( 'footer_email', 'fundamentwp' ); ?></a>
                         </p>
                     </div>
                 </div><!-- .footer-widget -->
@@ -49,10 +43,10 @@ $container = get_theme_mod('theme_layout_container', 'container');
                 </div><!-- .footer-widget -->
                 <div class="footer-widget col-lg-4 col-md-3 col-sm-12">
                     <div class="textwidget custom-html-widget">
-                        <h6>HELPDESK</h6>
-                        <p>ACDH-CH runs a helpdesk offering advice for questions related to various digital humanities topics.</p>
+                        <h6><?php _e( 'footer_helpdesk', 'fundamentwp' ); ?></h6>
+                        <p><?php _e( 'footer_helpdesk_text', 'fundamentwp' ); ?></p>
                         <p>
-                            <a class="helpdesk-button" href="mailto:acdh-helpdesk@oeaw.ac.at">ASK US!</a>
+                            <a class="helpdesk-button" href="mailto:acdh-helpdesk@oeaw.ac.at"><?php _e( 'footer_ask_usk', 'fundamentwp' ); ?></a>
                         </p>
                     </div>
                 </div><!-- .footer-widget -->
